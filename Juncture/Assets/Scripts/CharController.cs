7973862f7ed6cbce10b8de66289b061d501093b8
BuildTools <unconfigured@null.spigotmc.org>
2016-11-23 20:00:30 -0500

    Initial Build

---
+++ Juncture/Assets/Scripts/CharController.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+		Animator anim;
+
+		Rigidbody2D body;
+
+		System.Random r = new System.Random();
+		int start;
+		int current;
+		bool mov;
+		bool running;
+		int diff;
+		int state;
+		int level;
+		int i;
+		int upper;
+		public int orgin;
+		public int[] levels = new int[0];
+
+	// Use this for initialization
+	void Start () {
+
+				System.Timers.Timer aTimer = new System.Timers.Timer(1000);
+				aTimer.Elapsed += new System.Timers.ElapsedEventHandler (tick);
+				aTimer.Enabled=true;
+				anim = GetComponent<Animator> ();
+
+				body = GetComponent<Rigidbody2D> ();
+
+				upper = levels.Length;
+				level = orgin;
+
+				//body.freezeRotation = true;
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+				float x;
+				float y;
+				float factor = 3.0f;
+
+				x = Input.GetAxisRaw ("Horizontal");
+				y = Input.GetAxisRaw ("Vertical");
+		
+				if (y < 0) {
+						anim.SetBool ("MoveDown", true);
+				} else {
+						anim.SetBool ("MoveDown", false);	
+				}
+
+				if (y > 0) {
+						anim.SetBool("MoveUp", true);
+				}else {
+						anim.SetBool ("MoveUp", false);	
+				}
+
+				if (x > 0) {
+						anim.SetBool("MoveRight", true);
+				}else {
+						anim.SetBool ("MoveRight", false);	
+				}
+
+				if (x < 0) {
+						anim.SetBool("MoveLeft", true);
+				}else {
+						anim.SetBool ("MoveLeft", false);	
+				}
+
+				if ((x != 0) || (y != 0)) {
+						anim.SetBool ("Moving", true);
+						mov = true;
+						running = false;
+				} else {
+						anim.SetBool ("Moving", false);
+						mov = false;
+				}
+
+				Vector2 v = new Vector2(x,y);
+				body.velocity = v * factor;
+
+
+
+				if(mov == false){
+						if (running) {
+								diff = current - start;
+								if (diff < 0) {
+										diff += 60;
+								}
+								if (diff >= 3) {
+										state = r.Next (1, 4);
+								
+										switch (state) {
+										case 1:
+												anim.SetTrigger ("idle1");
+												break;
+										case 2:
+												anim.SetTrigger ("idle2");
+												break;
+										case 3:
+												anim.SetTrigger ("idle3");
+												break;
+										}
+										start = current;
+
+								}
+
+						} else {
+							running = true;
+							start = current;
+						}
+
+
+				}
+
+				if (Input.GetKeyDown(KeyCode.Q)) {
+
+						if (i - 1 > -1) {
+								i--;
+								print ("i" + i);
+								level = levels [i];
+								getAnim (level);
+								body.transform.Translate (0f, -100f, 0f);
+						}
+
+				}
+				if (Input.GetKeyDown(KeyCode.E)) {
+
+						if(i+1 < upper){
+								i++;
+								print ("i" + i);
+								level = levels [i];
+								getAnim (level);
+								body.transform.Translate (0f, 100f, 0f);
+						}
+
+
+				}
+
+	}
+
+		private void getAnim(int level){
+				print ("Level" + level);
+				switch (level) {
+
+				case 11:
+						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L1_Char/Char1_Light1") as RuntimeAnimatorController;	
+						break;
+				case 12:
+						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L2_Char/Char1_Light2") as RuntimeAnimatorController;
+						break;
+				case 21:
+						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C2_Char/C2_L1_Char/Char2_Light1") as RuntimeAnimatorController;
+						break;
+				}
+
+		}
+
+		private void tick(object sender, System.Timers.ElapsedEventArgs e)
+		{
+				current++;
+		}
+}
---
+++ Juncture/Assets/Scripts/TurningMech.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurningMech : MonoBehaviour {
+
+		SpriteRenderer spr;
+
+		public Dictionary<int, Sprite> dictSprites = new Dictionary<int, Sprite>();
+		public Sprite[] textures;
+		public Sprite texture1;
+		public Sprite texture2;
+
+	// Use this for initialization
+	void Start () {
+	
+				Sprite[] textures = Resources.LoadAll<Sprite> ("Textures/Sprites/TurningMech");
+				texture1 = Resources.Load<Sprite> ("Assets/Textures/Sprites/TurningMech_0");
+				texture2 = Resources.Load<Sprite> ("Assets/Textures/Sprites/TurningMech_1");
+				int i = 0;
+				foreach (Sprite s in textures) {
+						dictSprites.Add (i, s);
+						i++;
+				}
+				spr = GetComponent<SpriteRenderer> ();
+				print (textures.Length);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+				if(Input.GetKeyDown(KeyCode.Alpha1)){spr.sprite = dictSprites[0];}
+				if(Input.GetKeyDown(KeyCode.Alpha2)){spr.sprite = dictSprites[1];}
+				if(Input.GetKeyDown(KeyCode.Alpha3)){spr.sprite = dictSprites[2];}
+
+}
+}
