371318f4f628eceda9cb5ff8c1e7bdd1b47947ea
gmc1221 <grahamcooke1221@gmail.com>
2016-11-24 00:33:44 -0500

    Implemented Half of the New Sprites
    I’m leaving the other half for when the back animations are finished so
    I won’t have to redo the animation controllers all over gain

--- Juncture/Assets/Scripts/CharController.cs
+++ Juncture/Assets/Scripts/CharController.cs
@@ -41,106 +41,114 @@ public class CharController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-				float x;
-				float y;
-				float factor = 3.0f;
+		anim.SetInteger ("Idle", 0);
 
-				x = Input.GetAxisRaw ("Horizontal");
-				y = Input.GetAxisRaw ("Vertical");
+		float x;
+		float y;
+		float factor = 3.0f;
+
+		x = Input.GetAxisRaw ("Horizontal");
+		y = Input.GetAxisRaw ("Vertical");
+
+		if (y < 0) {
+			anim.SetBool ("MoveDown", true);
+			anim.SetInteger ("Facing", 0);
+		} else {
+			anim.SetBool ("MoveDown", false);	
+		}
+
+		if (y > 0) {
+			anim.SetBool("MoveUp", true);
+			anim.SetInteger ("Facing", 2);
+		}else {
+			anim.SetBool ("MoveUp", false);	
+		}
+
+		if (x > 0) {
+			anim.SetBool("MoveRight", true);
+			anim.SetInteger ("Facing", 3);
+		}else {
+			anim.SetBool ("MoveRight", false);	
+		}
+
+		if (x < 0) {
+			anim.SetBool("MoveLeft", true);
+			anim.SetInteger ("Facing", 1);
+		}else {
+			anim.SetBool ("MoveLeft", false);	
+		}
+
+		if ((x != 0) || (y != 0)) {
+			anim.SetBool ("Moving", true);
+			mov = true;
+			running = false;
+		} else {
+			anim.SetBool ("Moving", false);
+			mov = false;
+		}
+
+		Vector2 v = new Vector2(x,y);
+		body.velocity = v * factor;
+
+
+
+		if(mov == false){
+			if (running) {
+				diff = current - start;
+				if (diff < 0) {
+					diff += 60;
+				}
+				if (diff >= 3) {
+					state = r.Next (1, 4);
+					switch (state) {
+
+					case 1:
+						anim.SetInteger ("Idle", 1);
+						break;
+					case 2:
+						anim.SetInteger ("Idle", 2);
+						break;
+					case 3:
+						anim.SetInteger ("Idle", 3);
+						break;
+
+					}
+	
+					start = current;
 		
-				if (y < 0) {
-						anim.SetBool ("MoveDown", true);
-				} else {
-						anim.SetBool ("MoveDown", false);	
+				}
+	
+			} else {
+				running = true;
+				start = current;
+			}
+
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q)) {
+
+				if (i - 1 > -1) {
+						i--;
+						print ("i" + i);
+						level = levels [i];
+						getAnim (level);
+						body.transform.Translate (0f, -100f, 0f);
 				}
 
-				if (y > 0) {
-						anim.SetBool("MoveUp", true);
-				}else {
-						anim.SetBool ("MoveUp", false);	
+		}
+		if (Input.GetKeyDown(KeyCode.E)) {
+
+				if(i+1 < upper){
+						i++;
+						print ("i" + i);
+						level = levels [i];
+						getAnim (level);
+						body.transform.Translate (0f, 100f, 0f);
 				}
 
-				if (x > 0) {
-						anim.SetBool("MoveRight", true);
-				}else {
-						anim.SetBool ("MoveRight", false);	
-				}
 
-				if (x < 0) {
-						anim.SetBool("MoveLeft", true);
-				}else {
-						anim.SetBool ("MoveLeft", false);	
-				}
-
-				if ((x != 0) || (y != 0)) {
-						anim.SetBool ("Moving", true);
-						mov = true;
-						running = false;
-				} else {
-						anim.SetBool ("Moving", false);
-						mov = false;
-				}
-
-				Vector2 v = new Vector2(x,y);
-				body.velocity = v * factor;
-
-
-
-				if(mov == false){
-						if (running) {
-								diff = current - start;
-								if (diff < 0) {
-										diff += 60;
-								}
-								if (diff >= 3) {
-										state = r.Next (1, 4);
-								
-										switch (state) {
-										case 1:
-												anim.SetTrigger ("idle1");
-												break;
-										case 2:
-												anim.SetTrigger ("idle2");
-												break;
-										case 3:
-												anim.SetTrigger ("idle3");
-												break;
-										}
-										start = current;
-
-								}
-
-						} else {
-							running = true;
-							start = current;
-						}
-
-
-				}
-
-				if (Input.GetKeyDown(KeyCode.Q)) {
-
-						if (i - 1 > -1) {
-								i--;
-								print ("i" + i);
-								level = levels [i];
-								getAnim (level);
-								body.transform.Translate (0f, -100f, 0f);
-						}
-
-				}
-				if (Input.GetKeyDown(KeyCode.E)) {
-
-						if(i+1 < upper){
-								i++;
-								print ("i" + i);
-								level = levels [i];
-								getAnim (level);
-								body.transform.Translate (0f, 100f, 0f);
-						}
-
-
-				}
+		}
 
 	}
 
@@ -149,13 +157,13 @@ public class CharController : MonoBehaviour {
 				switch (level) {
 
 				case 11:
-						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L1_Char/Char1_Light1") as RuntimeAnimatorController;	
+						//anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L1_Char/Char1_Light1") as RuntimeAnimatorController;	
 						break;
 				case 12:
-						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L2_Char/Char1_Light2") as RuntimeAnimatorController;
+						//anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C1_Char/C1_L2_Char/Char1_Light2") as RuntimeAnimatorController;
 						break;
 				case 21:
-						anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C2_Char/C2_L1_Char/Char2_Light1") as RuntimeAnimatorController;
+						//anim.runtimeAnimatorController = Resources.Load ("Textures/Sprites/Entities/Char/C2_Char/C2_L1_Char/Char2_Light1") as RuntimeAnimatorController;
 						break;
 				}
 
